28db65c6214c39bf9bfbf63770acec5d0cb8e064
valenciapranata <68654127+valenciapranata@users.noreply.github.com>
2021-11-30 23:34:51 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+/*!
+* Start Bootstrap - Blog Home v5.0.7 (https://startbootstrap.com/template/blog-home)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+$(document).ready(function(){
+
+    var quantity=0;
+       $('.quantity-right-plus').click(function(e){
+            
+            // Stop acting like a button
+            e.preventDefault();
+            // Get the field name
+            var quantity = parseInt($('#quantity').val());
+            
+            // If is not undefined
+                
+                $('#quantity').val(quantity + 1);
+    
+              
+                // Increment
+            
+        });
+    
+         $('.quantity-left-minus').click(function(e){
+            // Stop acting like a button
+            e.preventDefault();
+            // Get the field name
+            var quantity = parseInt($('#quantity').val());
+            
+            // If is not undefined
+          
+                // Increment
+                if(quantity>0){
+                $('#quantity').val(quantity - 1);
+                }
+        });
+        
+    });
